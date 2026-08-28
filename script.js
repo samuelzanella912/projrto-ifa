d6f64049008a57be6629a1e9e41738cd0ca5330c
samuelzanella912 <samuel.zanella.costa@escola.pr.gov.br>
2026-08-28 10:54:30 -0300

    Update script.js

--- script.js
+++ script.js
@@ -255,15 +255,20 @@ function trocarImagem(escolha) {
     const imagemnova=document.getElementById('imagemPrincipal');
     switch(escolha){
         case "1":
-          urlNova = 'café.png';
-          textoprincipal.innerText="Como explicar o Sol?";
-          texto.innerText="O Sol é uma estrela gigante feita de gás quente que fica no centro do nosso Sistema Solar, a 150 milhões de quilômetros da Terra. Ele dá luz e calor ao nosso planeta, controla a gravidade que segura os planetas ao seu redor e é essencial para a existência de toda a vida.";
+          urlNova ='https://humanidades.com/br/astronomia/'
+          textoprincipal.innerText="O que é a astronomia";
+          texto.innerText="A astronomia é a ciencia do estudo das estrelas, dos planetas, das galaxias, das nebulosas, dos asteroides, dos cometas e de outros fenomenos cosmicos. Busca compreender a formação e a evolução das estrelas e dos planetas, conhecer a origem do universo, estudar a estrutura e a dinamica das galaxias e investigar a natureza dos buracos negros e da materia escura que formam parte do espaço";
           descrevefigura.innerText="Imagem do Sol na cor amarela ";
-          break;
         case "2":
+          urlNova = '';
+          textoprincipal.innerText="A verdadeira cor do SOL ";
+          texto.innerText="paragrafo";
+          descrevefigura.innerText="Imagem do Sol na cor branca ";
+          break;
+        case "3":
           urlNova = 'cordosol.jfif';
           textoprincipal.innerText="A verdadeira cor do SOL ";
-          texto.innerText="O Sol visto a olho nu no espaço é na verdade branco. A coloração amarela que vemos aqui na superfície da Terra é por causa da atmosfera da Terra.";
+          texto.innerText="paragrafo";
           descrevefigura.innerText="Imagem do Sol na cor branca ";
           break;
         default:
